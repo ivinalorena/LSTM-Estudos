5ff4d4726246bc7b541b919af38c235ce459575c
Ivina Lorena Oliveira Moura <108935037+ivinalorena@users.noreply.github.com>
2025-04-26 18:11:42 -0300

    Update lstminfodengue.py

--- fortaleza_lstm/lstminfodengue.py
+++ fortaleza_lstm/lstminfodengue.py
@@ -21,33 +21,20 @@ valor_real = []
 window_size = 4 # Tamanho da janela de tempo 4 semanas =  (~1 mês) // para para prever com os dados de 3 meses = 12
 
 for i in range(window_size,len(df_normalizado)):
-    # Cria a janela de tempo
-    #não entendi essa linha
+
     janela = df_normalizado[i-window_size:i, 0]
-    # Adiciona a previsão
     previsao.append(janela)
-    # Adiciona o valor real
     valor_real.append(df_normalizado[i, 0])
 
 # Converte as listas para arrays numpy
 previsao = np.array(previsao)
 valor_real = np.array(valor_real)
 
-# Reshape para [samples, time steps, features]
-#ajusta o formato dos dados para o modelo LSTM
-# O modelo LSTM espera os dados de entrada na forma [amostras, passos de tempo, características]
+
 previsao = np.reshape(previsao, (previsao.shape[0], previsao.shape[1], 1))
 
 
-#separar o conjunto de treinamento
-# O conjunto de treinamento é a parte dos dados que será usada para treinar o modelo.
-# Ele é usado para ajustar os pesos do modelo durante o processo de treinamento.
-# O modelo aprende a partir desses dados, ajustando seus parâmetros para minimizar a diferença entre as previsões do modelo e os valores reais.
-# Isso é feito através de um processo iterativo, onde o modelo faz previsões, calcula o erro e ajusta os pesos com base nesse erro.
-# O conjunto de teste é a parte dos dados que será usada para avaliar o desempenho do modelo após o treinamento.
-#separar o conjunto de teste
-# O conjunto de teste é a parte dos dados que não foi usada durante o treinamento do modelo.
-#separação dos dados em treino e teste
+
 tam_treinamento = int(len(df_normalizado) * 0.8) # 80% dos dados para treinamento e 20% para teste
 X_treinamento = previsao[:tam_treinamento]
 x_teste = previsao[tam_treinamento:]
@@ -68,17 +55,10 @@ modelo.add(Dropout(0.3))
 
 modelo.add(Dense(units=1)) # Camada de saída com 1 unidade
 modelo.compile(optimizer='adam', loss='mean_squared_error',metrics=['mean_absolute_error', RootMeanSquaredError()])
-#Batch size (tamanho do lote) é um hiperparâmetro importante no treinamento de modelos de machine learning, 
-# especialmente em redes neurais. Ele define o número de amostras que serão processadas antes de atualizar os parâmetros (pesos) do modelo.
-#verbose=0: Modo silencioso. Não exibe nenhuma informação durante o processo.
-#verbose=1: Modo padrão/normal. Mostra uma barra de progresso e métricas básicas durante o treinamento (como loss, accuracy) em cada época.
-#verbose=2: Modo mais detalhado. Geralmente mostra informações sobre cada época, mas sem a barra de progresso.
-#modelo.fit treina o modelo com os dados de entrada (previsao) e os valores reais (valor_real).
-# O parâmetro batch_size define quantas amostras serão processadas antes de atualizar os pesos do modelo.
-# O parâmetro epochs define quantas vezes o modelo verá todo o conjunto de dados durante o treinamento.
+
 modelo.fit(X_treinamento, y_treinamento, batch_size=32, epochs=100, verbose=1)
 
-# Previsão no conjunto de TREINAMENTO (novo)
+
 previsao_treinamento_lstm = modelo.predict(X_treinamento)
 previsao_treinamento_desnormalizada = normalizador.inverse_transform(previsao_treinamento_lstm)
 y_treinamento_desnormalizado = normalizador.inverse_transform(y_treinamento.reshape(-1, 1))
@@ -89,8 +69,6 @@ previsao_lstm = modelo.predict(x_teste)
 previsao_teste_desnormalizada = normalizador.inverse_transform(previsao_lstm)
 y_teste_desnormalizado = normalizador.inverse_transform(y_teste.reshape(-1, 1))
 
-#print("Previsão LSTM: ", previsao_desnormalizada)
-#print("Valor Real: ", y_teste_desnormalizado)
 
 # Cálculo de métricas para TREINAMENTO 
 mae_treinamento = mean_absolute_error(y_treinamento_desnormalizado, previsao_treinamento_desnormalizada)
@@ -115,17 +93,7 @@ print("\nMétricas de TESTE:")
 print(f"MAPE (Teste): {mape_teste:.2f}%")
 print(f"RMSE (Teste): {rmse_teste:.2f}")
 
-#plotar os resultados
-#plt.figure(figsize=(12, 6))
-#plt.plot(normalizador.inverse_transform(df_normalizado), color='blue', label='Dados Reais')
-#plt.plot(previsao_desnormalizada, color='red', label='Previsão LSTM')
-#plt.plot(y_teste_desnormalizado, color='green', label='Valor Real Teste')
-#plt.axvline(x=tam_treinamento, color='gray', linestyle='--', label='Divisão Treino/Teste')
-#plt.title('Previsão de Casos de Dengue')
-#plt.xlabel('Dias')
-#plt.ylabel('Casos de Dengue')
-#plt.legend()
-#plt.show()
+
 
 # Plotando os resultados
 plt.figure(figsize=(14, 6))
@@ -141,8 +109,6 @@ plt.plot(train_range, previsao_treinamento_desnormalizada, color='orange', label
 test_range = range(tam_treinamento, tam_treinamento+len(previsao_teste_desnormalizada))
 plt.plot(test_range, previsao_teste_desnormalizada, color='red',  label='Previsão (Teste)')
 
-# Valores reais de TESTE
-#plt.plot(test_range, y_teste_desnormalizado, color='green', linewidth=2, label='Valores Reais (Teste)')
 
 plt.axvline(x=tam_treinamento, color='black', linestyle='--', label='Divisão Treino/Teste')
 plt.title('Comparação: Previsões no Treino e Teste')
@@ -151,9 +117,3 @@ plt.ylabel('Casos de Dengue')
 plt.legend()
 plt.grid(True)
 plt.show()
-# Títulos e legendas
-#plt.title('Previsão de Casos de Dengue')
-#plt.xlabel('Tempo')
-#plt.ylabel('Casos de Dengue')
-#plt.legend()
-#plt.show()
